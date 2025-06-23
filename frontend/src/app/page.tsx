3373a771cbb9d06eb472b2459b49ad4a0e3c9611
Classic-German <175220123+Classic-German@users.noreply.github.com>
2025-06-23 15:11:35 +0100

    Finish SWR to all fruits on table

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -13,19 +13,13 @@ type Fruit = {
   description: string;
 };
 
-const fetcher = (url) => fetch(url).then((res) => res.json());
-
 const Fruits = () => {
- const { data, error } = useSWR("http://localhost:8080/fruits", fetcher);
-  // useEffect(() => {
-  //   fetch("http://localhost:8080/fruits")
-  //     .then((res) => res.json())
-  //     .then((data) => setFruits(data));
-  // }, []);
+  const fetcher = (url) => fetch(url).then((res) => res.json());
+  const { data } = useSWR("http://localhost:8080/fruits", fetcher, {revalidateOnFocus: true});
 
   const columns = ["Ícone", "Nome", "Vitamina", "CFT"];
   const router = useRouter();
- 
+
   const handleClick = (id: number) => {
     router.push(`/fruit-page/${id}`);
   };
@@ -40,65 +34,58 @@ const Fruits = () => {
       fetch(`http://localhost:8080/fruits/${id}`, { 
       method: "DELETE",
     })
-      .then(() => console.log("Fruta eliminada com sucesso!"))
       .then(() => window.location.reload()) // Reload to the fruits page after submission
       .catch((error) => console.error(error));
     }
   }
 
-
-
-
   return (
-      <div className={styles.container}>
-        <div className={styles.content}>
-          <div className={styles.title}>
-            <h1>Tudo o que precisa saber sobre estas frutas!</h1>
-            <h2>Clica numa fruta para saber as suas características</h2>
-          </div>
-          <div className={styles.tableContainer}> 
-            {!data ? <div>Loading...</div> : 
-            <table className={styles.table}>
-              <thead>
-                <tr>
-                  {columns.map((column, i) => (
-                    <th className={styles.th} key={i}  >
-                      {column}
-                    </th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-
-                  {error && <div>Erro ao carregar</div>}
- 
-                {data.map((fruit: Fruit) => (
-                  <tr
-                    key={fruit.id}
-                    onClick={() => handleClick(fruit.id)}
-                    className={styles.tr}
-                  >
-                    <td className={styles.td}>{fruit.icon}</td>
-                    <td className={styles.td}>{fruit.name}</td>
-                    <td className={styles.td}>{fruit.vitamin}</td>
-                    <td className={styles.td}>{fruit.cft ? "Sim" : "Não"}</td>
-                    <td onClick={e => { e.stopPropagation(); handleDeleteFruit(fruit.id); }}><IconTrashXFilled/></td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>}
-          </div>
-          <p className={styles.newFruit}>
-            A sua fruta favorita não está aqui? Adicione-a!
-            <button
-              className={styles.adicionarFrutabtn}
-              onClick={handleAddFruit}
-            >
-              Adicionar Fruta
-            </button>
-          </p>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.title}>
+          <h1>Tudo o que precisa saber sobre estas frutas!</h1>
+          <h2>Clica numa fruta para saber as suas características</h2>
         </div>
+        <div className={styles.tableContainer}> 
+          {!data ? <div>Loading...</div> : 
+          <table className={styles.table}>
+            <thead>
+              <tr>
+                {columns.map((column, i) => (
+                  <th className={styles.th} key={i}  >
+                    {column}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((fruit: Fruit) => (
+                <tr
+                  key={fruit.id}
+                  onClick={() => handleClick(fruit.id)}
+                  className={styles.tr}
+                >
+                  <td className={styles.td}>{fruit.icon}</td>
+                  <td className={styles.td}>{fruit.name}</td>
+                  <td className={styles.td}>{fruit.vitamin}</td>
+                  <td className={styles.td}>{fruit.cft ? "Sim" : "Não"}</td>
+                  <td onClick={e => { e.stopPropagation(); handleDeleteFruit(fruit.id); }}><IconTrashXFilled/></td>
+                </tr>
+              ))}
+            </tbody>
+          </table>}
+        </div>
+        <p className={styles.newFruit}>
+          A sua fruta favorita não está aqui? Adicione-a!
+          <button
+            className={styles.adicionarFrutabtn}
+            onClick={handleAddFruit}
+          >
+            Adicionar Fruta
+          </button>
+        </p>
       </div>
+    </div>
   );
 };
 
